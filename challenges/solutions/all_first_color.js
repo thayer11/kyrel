c97d4b0dce1dec28b4582d59e10cf0f8454a2e84
tgaff <tgaff@users.noreply.github.com>
2016-03-16 12:53:45 -0700

    Update all_first_color.js

--- challenges/solutions/all_first_color.js
+++ challenges/solutions/all_first_color.js
@@ -18,3 +18,34 @@ for(var j=0; j<5; j++) {
   draw();
   moveRight();
 }
+
+
+/* 
+Note that the above solution actually moves off the Kyrel board.  Maybe that's OK.
+
+Sometimes in software development the instructions you're given are not 100% clear.  There are frequently edge-cases, or other possibilities that hadn't been thought of yet.
+You may have to improvise or ask questions.  That's OK.  Considering those other possibilities is actually part of the job of a good engineer.
+
+A possible alternative would be to change the solution to moveRight before drawing.  We don't really need to draw the first square anyway.
+*/
+
+/*
+For example:
+
+if ( onBlue() ) {
+  useBlue()
+} else if ( onGreen() ) {
+  useGreen();
+}
+
+for(var j=0; j<4; j++) {
+  moveRight();
+  draw();
+}
+
+*/
+
+/*
+Additional challenge:  What if the first square is empty?  Should we move that to the end as well and make the last square empty?  
+Can you think of a solution for this too?
+*/
