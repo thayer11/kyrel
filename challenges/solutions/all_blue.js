59abbcdf5916d9744b1f8bdd7ea754b75854ecff
nathanallen <thenathanator@gmail.com>
2016-03-17 11:16:49 -0700

    add alternate accepted solution

--- challenges/solutions/all_blue.js
+++ challenges/solutions/all_blue.js
@@ -1,12 +1,30 @@
-// initial_state = [ 'b', 'b', '.', 'g', 'b' ];
-// end_state =     [ 'g', 'g', '.', 'b', 'g' ];
+// initial_state = ['.', '.', '.', '.', '.'];
+// end_state =     ['b', 'b', 'b', 'b', 'b'];
 
 //////////
 // main //
 //////////
 
 useBlue();
-for(var j=0; j<5; j++) {
+for(var j=0; j<5; j++) { // 5 loops
+  draw();
+  moveRight(); // on the 5th loop we "fall" off the row!
+}
+
+//* alternate solution *//
+
+useBlue();
+draw(); // draw first
+for(var i=0; i<4; i++) { // 4 loops
+  moveRight();
+  draw();
+}
+
+//* alternate solution *//
+
+useBlue();
+for(var l=0; l<4; l++) { // 4 loops
   draw();
   moveRight();
 }
+draw(); // draw last
